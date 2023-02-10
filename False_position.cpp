8eb1f2d8be44d862eca77fa9bcfab439e9931121
rakib3903 <rakibhossain3903@gmail.com>
2023-02-10 11:31:11 +0600

    Founded the root using fasle position method.

---
+++ False_position.cpp
@@ -0,0 +1,53 @@
+    #include<bits/stdc++.h>
+    #define ll long long
+    using namespace std;
+    
+    class BisectionMethod{
+
+      private:
+      double f(double x){
+        return x*x*x + x*x - 1;
+      }
+
+      public:
+      double a, b;
+      BisectionMethod(){
+        for(int i = -9; i <= 9; i++){
+          for(int j = -9; j<=9; j++){
+            if(f(i) * f(j) < 0){
+              a = min(i,j);
+              b = max(i, j);
+            }
+          }
+        }
+      }
+      void ab(){
+        double pricision = 0.0001;
+      double c = ((a * f(b)) - (b * f(a))) /(f(b) - f(a));
+      double x = c;
+          if(f(a) * f(c) > 0){
+                    a = c;
+                  }else{
+                    b = c;
+                  }
+                  c = ((a * f(b)) - (b * f(a))) /(f(b) - f(a));
+      while(true){
+        if(c == 0.0) break;
+         else if(f(a) * f(c) > 0){
+          a = c;
+        }else{
+          b = c;
+        }
+        if(abs(c - x) <= pricision) break;
+        x = c;
+        c = ((a * f(b)) - (b * f(a))) /(f(b) - f(a));
+      }
+      cout<<c<<endl;
+      }
+    };
+
+    int main(){  
+      BisectionMethod bsm;
+      bsm.ab();
+      return 0;
+    }
