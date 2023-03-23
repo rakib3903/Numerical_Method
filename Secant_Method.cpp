c54e5fdff4ec723443aa27bcf9495765b013a9de
rakib3903 <rakibhossain3903@gmail.com>
2023-03-23 12:01:14 +0600

    Newton forward interpolation is implemented.

--- Secant_Method.cpp
+++ Secant_Method.cpp
@@ -1,10 +1,10 @@
     #include<bits/stdc++.h>
     #define ll long long
     using namespace std;
-    
+
     double f(double x){
-        return x*x*x + x*x - 1;
-      }
+        return pow(x,2) - x - 2;
+    }
 
     class Secant_Method{
         public:
@@ -19,21 +19,27 @@
                 }
             }
         }
+
         void ab(){
             double pricision = 0.001;
+            double x;
             while(true){
-                double x = ((a * f(b)) - b * f(a))/(f(b) - f(a));
+                x = ((a * f(b)) - b * f(a))/(f(b) - f(a));
                 cout<<"roots----> "<<a<<" "<<b<<" "<<x<<endl;
                 if(abs((x - b)/x) < pricision) break;
                 a = b;
                 b = x;
             }
-            cout<<b<<endl;
+            cout<<x<<endl;
         }
+
+
     };
 
     int main(){  
       Secant_Method sm;
       sm.ab();
+      double p = (double)22 / (double)7;
+      cout<<p<<endl;
       return 0;
     }
---
+++ newtonForwardDifference.cpp
@@ -0,0 +1,58 @@
+    #include<bits/stdc++.h>
+    #define ll long long
+    using namespace std;
+    
+    int fact(int n){
+        int p = 1;
+        for(int i = 1; i <= n; i++){
+            p *= i;
+        }
+        return p;
+    }
+
+    double divid(int i, double v){
+        double p = 1.0;
+        for(int j = 0; j < i; j++){
+            p *= (v - j);
+        }
+        return p;
+    }
+
+    class newtonForwardDifference{
+      public:
+      int n;
+      double x1;
+      double x[10000];
+      double  y[1000][1000];
+
+      newtonForwardDifference(){
+        cin>>n;
+        for(int i = 0; i < n; i++){
+            cin>>x[i];
+        }
+        for(int i = 0; i < n; i++){
+            cin>>y[0][i];
+        }
+        cin>>x1;
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < n-i; j++){
+                y[i][j] = y[i-1][j+1] - y[i-1][j];
+            }
+        }
+      }
+
+      double solve(){
+        double ans = y[0][0];
+        double u = (x1 - x[0]) / (x[1] - x[0]);
+        for(int i = 1; i < n; i++){
+            ans += (divid(i, u) * y[i][0]) / fact(i);
+        }
+        return ans;
+      }
+    };
+
+    int main(){  
+      newtonForwardDifference bsm;
+      cout<<bsm.solve()<<endl;
+      return 0;
+    }
